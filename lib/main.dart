a1ebc27467cda8d6e0f24900a00ce37438a2edfd
Riya Gandhi <riya@hudle.in>
2025-05-13 00:10:31 +0530

    Add: init

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(255,78,13,151),
+            Color.fromARGB(255,107,13,151)
+          ])
+        ),
+        
+        child: const StartScreen()),
+    ),
+  )); // contains root widget
+}
